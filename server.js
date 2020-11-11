33a138ed12cfc138ad67bed83bedd76d4c9a2a0d
unknown <mahmoudelkhadragy38@gmail.com>
2020-11-11 10:51:55 +0200

    get product/id

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 import express from "express";
 import { data } from "./data.js";
-const port = process.env.PORT || 5000;
 const app = express();
+
+//
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find(
+    (product) => product._id === req.params.id
+  );
+  if (!product) return res.status(404).send({ message: "Product not found" });
+
+  res.send(product);
+});
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
@@ -11,6 +20,7 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
+const port = process.env.PORT || 9000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
